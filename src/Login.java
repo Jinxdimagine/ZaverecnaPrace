1b6dbc93be01eec9744efbf40a6f57e986ac1021
Jinxdimagine <martinvu403@gmail.com>
2024-03-22 11:40:20 +0100

    Created Class Login with basic settings

---
+++ src/Login.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+    Login(){
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setBounds(200,200,600,600);
+
+    }
+}
