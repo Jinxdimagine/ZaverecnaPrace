e1cf467788cbc452d3b14914e67d2d8e857ea8b8
Jinxdimagine <martinvu403@gmail.com>
2024-05-14 16:18:40 +0200

    Create method that add textfield for user to sign in.

--- src/Login.java
+++ src/Login.java
@@ -1,12 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Login extends JFrame {
 
     Login(){
         this.setResizable(false);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setVisible(true);
         this.setBounds(200,200,600,600);
+        this.setLayout(null);
+        addTextfield();
+        this.setVisible(true);
+    }
 
+    void addTextfield(){
+        Panel panel=new Panel();
+        JTextField username = new JTextField();
+        JTextField password = new JTextField();
+        JLabel TextUsername=new JLabel("Username");
+        JLabel TextPassword=new JLabel("Password");
+        panel.setBounds(50,50,300,80);
+        panel.setLayout(new GridLayout(2,2));
+        panel.add(TextUsername);
+        panel.add(username);
+        panel.add(TextPassword);
+        panel.add(password);
+        this.add(panel);
     }
 }
