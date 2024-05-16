a12a506649a0bee87a0e7b9d3d9803aba3f147e1
Jinxdimagine <martinvu403@gmail.com>
2024-05-16 17:50:57 +0200

    Added Getter and Setter to class Account.

--- src/Account.java
+++ src/Account.java
@@ -5,4 +5,44 @@ public class Account {
 
     private String UserName;
     private String Password;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
