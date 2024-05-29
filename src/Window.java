670bf6fa5a1bec513c12b9f6f290ace4686fd922
Jinxdimagine <martinvu403@gmail.com>
2024-05-29 23:14:57 +0200

    fix

--- src/Window.java
+++ src/Window.java
@@ -4,13 +4,20 @@ public class Window extends JFrame {
 
     private Database database;
 
-    public Window(Database database) {
+    private Account account;
+
+    public Window(Database database,Account account) {
         this.setResizable(false);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setBounds(200, 200, 600, 600);
         this.setLayout(null);
         setDatabase(database);
+        setAccount(account);
         this.setVisible(true);
+    }
+
+    void addPanel(){
+        JPanel panel= new JPanel();
     }
 
     public Database getDatabase() {
@@ -19,5 +26,13 @@ public class Window extends JFrame {
 
     public void setDatabase(Database database) {
         this.database = database;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
     }
 }
